5b1f2e6060232e865d9fa6bb05202ace6ed242ab
OfeliaGarc <ofee.50.og@gmail.com>
2017-03-10 15:17:26 -0600

    Ultima version proyecto final

--- js/app.js
+++ js/app.js
@@ -2,50 +2,37 @@ var estudiantes = [];
 
 function obtenerListaEstudiantes() {
     // TO DO: Retornar la lista de estudiantes
-  /*  for(var i = 0; i < estudiantes.length; i ++)
-    estudiante ++;*/
+    return estudiantes;
 }
 
 function agregarEstudiante() {
     // TO DO: Preguntar al usuario por el nombre, puntos técnicos y puntos de HSE de un estudiante
     // Hint: El estudiante debe ser agregado a la lista de estudiantes
     // TO DO: Retornar el estudiante recientemente creado
-    var preguntas = [
-      "Nombre de la estudiante:",
-      "Porcentaje técnico",
-      "Porcentaje Habilidades Socio-Emocionales"
-    ];
 
-    for (var i = 0; i < preguntas.length ; i++){
-      var ingresarEstudiante = prompt (preguntas [i]); // arroja todas la preguntas
-      //estudiante = estudiante.toLowerCase();
-     }
+    var nombreEstudiante = prompt ("Nombre:"); // declarar variable de las preguntas
+    var puntosTecnicos = prompt ("Puntos Tecnicos:");
+    var puntosHse = prompt ("Puntos Habiliades Socio-Emocionales:");
 
+    var alumno = { //objeto que almacena los datos del alumno ingresado
+      "nombreEstudiante": nombreEstudiante,
+      "puntosTecnicos": puntosTecnicos,
+      "puntosHse": puntosHse
+    }
+    estudiantes.push (alumno); //guardar en el arreglo global
+    return alumno; //retorna el ultimo alumno ingresado
 }
 function mostrar(estudiante) {
     // TO DO: Completar el template para que muestre las propiedades correctas del estudiante
+
     var resultado = "";
-      estudiante = {
-         nombre: estudiante,
-         tecnicos: parseInt("tecnicos"),
-         hse: parseInt("hse")
-       }; // objeto con valores no definidos
-       /*estudiante.nombre = nombre;
-       estudiante.apellido = apellido;*/ //esto ayuda cuando no se sabe las propiedades del objeto y se agregan mas
-       estudiantes.push (estudiante); //push permite poner un elemento al final del arreglo
-    /* function estudiante(nombre, tecnico, hse)
-      {
-        this.nombre: nombre,
-        this.tecnico: tecnico,
-        this.hse: hse
-      }*/
     resultado += "<div class='row'>";
     resultado += "<div class='col m12'>";
     resultado += "<div class='card blue-grey darken-1'>";
     resultado += "<div class='card-content white-text'>";
-    resultado += "<p><strong>Nombre:</strong> " + estudiante.nombre + "</p>";
-    resultado += "<p><strong>Puntos Técnicos:</strong> " + estudiante.tecnicos + "</p>";
-    resultado += "<p><strong>Puntos HSE:</strong> " + estudiante.hse + "</p>";
+    resultado += "<p><strong>Nombre:</strong> " + estudiante.nombreEstudiante + "</p>"; //llama a la propiedad del objeto
+    resultado += "<p><strong>Puntos Técnicos:</strong> " + estudiante.puntosTecnicos + "</p>";
+    resultado += "<p><strong>Puntos HSE:</strong> " + estudiante.puntosHse + "</p>";
     resultado += "</div>";
     resultado += "</div>";
     resultado += "</div>";
@@ -58,23 +45,36 @@ function mostrar(estudiante) {
 function mostrarLista(estudiantes) {
     // TO DO: Iterar la lista del estudiantes para devolverlos en el formato que usa la función mostrar(estudiante)
     // Retornar el template de todos los estudiantes
-
-} /*puede se con un for que haga que los estudiantes que ingresaste vayan sumandose, o llando a la funcion que donde obtuve
-la lista para aqui imprimirla*/
+    var lista = ""; //variable que almacena los estudiantes que ingresaste
+    for(var i = 0; i < estudiantes.length; i++){ //recorre la longitud del arreglo estudiantes
+        lista += mostrar(estudiantes[i]); //concatenacion de la lista y los estudiantes ingresados llamando a mostrar
+    }
+    return lista;
+}
 
 function buscar(nombre, estudiantes) {
     // TO DO: Buscar el nombre en la lista de estudiantes que se recibe por parámetros
     // Retornar el objeto del estudiante buscado
     // Nota: NO IMPORTA SI EL USUARIO ESCRIBE EL NOMBRE EN MAYÚSCULAS O MINÚSCULAS
+    var estudiantesFiltrados = estudiantes.filter (function (alumno){
+      return alumno.nombreEstudiante.toLowerCase () == nombre.toLowerCase ();
+    });
+    return estudiantesFiltrados;
 
 }//filter de la lista final por el nombre y poner un toLowerCase
 
 function topTecnico(estudiantes) {
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje técnico de mayor a menor
-
+    var ordenarPuntosTecnicos = estudiantes.sort (function (itemA, itemB){
+      return itemB.puntosTecnicos > itemA.puntosTecnicos;
+    });
+    return ordenarPuntosTecnicos;
 }
 
 function topHSE(estudiantes) {
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje de HSE de mayor a menor
-
+    var ordenarPuntosHse = estudiantes.sort (function (itemA, itemB){
+      return itemB.puntosHse > itemA.puntosHse;
+    });
+    return ordenarPuntosHse;
 }
